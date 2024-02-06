7628c3fbcef0ada2fc7f6f37dd427a39c6c2f10b
mustafa-kamel <mostafak252@gmail.com>
2024-02-06 20:36:50 +0200

    Rearrange summerizer app and edit the response

--- app.py
+++ app.py
@@ -1,31 +1,8 @@
-import os
-
-from dotenv import load_dotenv
 from flask import Flask, jsonify, request
-from openai import OpenAI
 
-load_dotenv()
+from scripts.summerizer import summarize_blog
 
 app = Flask(__name__)
-client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-
-
-# Function to summarize the blog
-def summarize_blog(blog_text):
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": "Summarize the following blog in two paragraphs:\n",
-            },
-            {
-                "role": "user",
-                "content": blog_text,
-            },
-        ],
-    )
-    return response.choices[0].message.content
 
 
 # API endpoint for summarizing blog
@@ -33,8 +10,7 @@ def summarize_blog(blog_text):
 def summarize():
     data = request.get_json()
     summary = summarize_blog(data["blog_text"])
-    print(summary)
-    return jsonify({"summary": summary})
+    return jsonify(summary)
 
 
 if __name__ == "__main__":
---
+++ scripts/summerizer.py
@@ -0,0 +1,30 @@
+import json
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+
+# Function to summarize the blog
+def summarize_blog(blog_text):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": """You are a helpful assistant who reads blog posts and gives a summerization for them,
+                you give a short description for the items mentioned in the blog post for the first 5 items,
+                you return the summary in json format like the following:
+                {"summary": {"item 1": "paragraph 1", "item 2": "paragraph 2", ...}}""",
+            },
+            {
+                "role": "user",
+                "content": f"Summarize the blog delimited by the back ticks in two paragraphs:\n `{blog_text}`",
+            },
+        ],
+    )
+    return json.loads(response.choices[0].message.content)
