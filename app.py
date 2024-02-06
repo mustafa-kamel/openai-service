1a70635892f59f85a4aaaacf9bb9e3c9962a841f
mustafa-kamel <mostafak252@gmail.com>
2024-02-06 21:01:40 +0200

    Add recommendations endpoint

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 from flask import Flask, jsonify, request
 
+from scripts.recommend_engine import get_recommendations
 from scripts.summerizer import summarize_blog
 
 app = Flask(__name__)
 
 
-# API endpoint for summarizing blog
+# API endpoint for summarizing a blog
 @app.route("/summarize-blog", methods=["POST"])
 def summarize():
     data = request.get_json()
-    summary = summarize_blog(data["blog_text"])
+    summary = summarize_blog(data.get("blog_text"))
     return jsonify(summary)
+
+
+# API endpoint for getting product documentations
+@app.route("/recommendations", methods=["POST"])
+def recommendations():
+    data = request.get_json()
+    return jsonify(get_recommendations(data.get("query_string"), data.get("length", 3)))
 
 
 if __name__ == "__main__":
